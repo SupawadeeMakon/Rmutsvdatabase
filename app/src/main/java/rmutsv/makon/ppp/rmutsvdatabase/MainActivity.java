f115ed4179b07842f6ac5253dad47e6981f5a7b0
Supawadee-MAKON <supawadee018@gmail.com>
2017-07-03 10:56:44 +0700

    Initial commit

---
+++ app/src/main/java/rmutsv/makon/ppp/rmutsvdatabase/MainActivity.java
@@ -0,0 +1,13 @@
+package rmutsv.makon.ppp.rmutsvdatabase;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
