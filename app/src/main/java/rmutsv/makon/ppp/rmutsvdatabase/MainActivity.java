93a711e1fd76fa0d69fed66b7ef0cbe435cbc3a7
Supawadee-MAKON <supawadee018@gmail.com>
2017-07-03 15:43:26 +0700

    Design UI NewRegister

--- app/src/main/java/rmutsv/makon/ppp/rmutsvdatabase/MainActivity.java
+++ app/src/main/java/rmutsv/makon/ppp/rmutsvdatabase/MainActivity.java
@@ -3,11 +3,24 @@ package rmutsv.makon.ppp.rmutsvdatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import rmutsv.makon.ppp.rmutsvdatabase.fragment.MainFragment;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+        //Add Fragment
+        if (savedInstanceState == null) {
+            MainFragment mainFragment = new MainFragment();
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.relContent, mainFragment)
+                    .commit();
+        }
+
+
+    }//main method
+
+}//main class
